2528371f5c2d9c81198e099b9093f7891bf28657
Fran Zekan <zekan.fran369@gmail.com>
2020-12-31 02:03:02 +0100

    Playing with jest

--- app.test.js
+++ app.test.js
@@ -1,15 +1,48 @@
 const build = require("./app");
 
-const test = async () => {
-  const app = build();
+let app = undefined;
 
-  const res = await app.inject({
-    method: "GET",
-    url: "/",
+describe("App", () => {
+  beforeAll(async () => {
+    app = await build();
+    await app.ready();
   });
 
-  console.log(res.statusCode);
-  console.log(res.body);
-};
+  afterAll(() => {
+    app.close();
+  });
 
-test();
+  test("/", async () => {
+    const res = await app.inject({ method: "GET", url: "/" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.json()).toEqual({ hello: "world" });
+  });
+
+  test("/:name with random name", async () => {
+    const res = await app.inject({ method: "GET", url: "/foobar" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers["content-type"]).toEqual(
+      "application/json; charset=utf-8"
+    );
+  });
+
+  test("/:name with bar", async () => {
+    const res = await app.inject({ method: "GET", url: "/bar" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual("NO");
+    expect(res.headers["content-type"]).toEqual("text/plain; charset=utf-8");
+  });
+
+  test("/:name with foo", async () => {
+    const res = await app.inject({ method: "GET", url: "/foo" });
+    expect(res.statusCode).toEqual(500);
+    expect(res.json()).toEqual({
+      statusCode: 500,
+      error: "Internal Server Error",
+      message: "ERROR",
+    });
+    expect(res.headers["content-type"]).toEqual(
+      "application/json; charset=utf-8"
+    );
+  });
+});
