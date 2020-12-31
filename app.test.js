3038fb97bf5591a76300fb27acb2a407f3d399cc
Fran Zekan <zekan.fran369@gmail.com>
2020-12-31 02:13:02 +0100

    Reorganise jest testing stuff

--- app.test.js
+++ app.test.js
@@ -12,37 +12,41 @@ describe("App", () => {
     app.close();
   });
 
-  test("/", async () => {
-    const res = await app.inject({ method: "GET", url: "/" });
-    expect(res.statusCode).toEqual(200);
-    expect(res.json()).toEqual({ hello: "world" });
-  });
-
-  test("/:name with random name", async () => {
-    const res = await app.inject({ method: "GET", url: "/foobar" });
-    expect(res.statusCode).toEqual(200);
-    expect(res.headers["content-type"]).toEqual(
-      "application/json; charset=utf-8"
-    );
-  });
-
-  test("/:name with bar", async () => {
-    const res = await app.inject({ method: "GET", url: "/bar" });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual("NO");
-    expect(res.headers["content-type"]).toEqual("text/plain; charset=utf-8");
-  });
-
-  test("/:name with foo", async () => {
-    const res = await app.inject({ method: "GET", url: "/foo" });
-    expect(res.statusCode).toEqual(500);
-    expect(res.json()).toEqual({
-      statusCode: 500,
-      error: "Internal Server Error",
-      message: "ERROR",
+  describe("Home route", () => {
+    test("/", async () => {
+      const res = await app.inject({ method: "GET", url: "/" });
+      expect(res.statusCode).toEqual(200);
+      expect(res.json()).toEqual({ hello: "world" });
     });
-    expect(res.headers["content-type"]).toEqual(
-      "application/json; charset=utf-8"
-    );
+  });
+
+  describe("Name route", () => {
+    test("/:name with bar", async () => {
+      const res = await app.inject({ method: "GET", url: "/bar" });
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toEqual("NO");
+      expect(res.headers["content-type"]).toEqual("text/plain; charset=utf-8");
+    });
+
+    test("/:name with foo", async () => {
+      const res = await app.inject({ method: "GET", url: "/foo" });
+      expect(res.statusCode).toEqual(500);
+      expect(res.json()).toEqual({
+        statusCode: 500,
+        error: "Internal Server Error",
+        message: "ERROR",
+      });
+      expect(res.headers["content-type"]).toEqual(
+        "application/json; charset=utf-8"
+      );
+    });
+
+    test("/:name with random name", async () => {
+      const res = await app.inject({ method: "GET", url: "/foobar" });
+      expect(res.statusCode).toEqual(200);
+      expect(res.headers["content-type"]).toEqual(
+        "application/json; charset=utf-8"
+      );
+    });
   });
 });
